06db2e3e57afb1363fa93cb117be098dfda38f54
kimsongsub <busanbak2000@naver.com>
2022-01-07 15:29:47 +0900

    Add Sign-UP

--- js/check-user.js
+++ js/check-user.js
@@ -1,8 +1,11 @@
 const loginName = document.querySelectorAll(".login-form input");
 const loginForm = document.querySelector(".login-form");
+const signupForm = document.querySelector(".signup-form");
 const loggedInPage = document.querySelector(".loggedin-Page");
 
 let userNumber = 1;
+
+//Log-in form
 function checkUserName(event) {
   event.preventDefault();
   const userName = loginName[0].value;
@@ -14,12 +17,35 @@ function checkUserName(event) {
     // userNumber += 1;
     alert("There is no Name Here");
   } else if (localStorage.getItem(userName) !== userPW) {
-    alert("Permisstion Denied");
+    alert("Wrong PW, Permisstion Denied");
   } else {
     loginForm.classList.add(HIDE_ELEMENT_CLASS);
+    signupForm.classList.add(HIDE_ELEMENT_CLASS);
     loggedInPage.classList.remove(HIDE_ELEMENT_CLASS);
     loggedInPage.innerText = `Welcome ${userName}`;
   }
 }
 
-document.addEventListener("submit", checkUserName);
+loginForm.addEventListener("submit", checkUserName);
+
+//Sign-up form
+function signupUserInfo(event) {
+  event.preventDefault();
+  const undifinedUserIDInput = signupForm.querySelector("#signup-id");
+  const undifinedUserPWInput = signupForm.querySelector("#signup-pw");
+
+  if (localStorage.getItem(undifinedUserIDInput.value) === null) {
+    localStorage.setItem(
+      undifinedUserIDInput.value,
+      undifinedUserPWInput.value
+    );
+    alert(`Sign-up Success. Try Login with "${undifinedUserIDInput.value}"`);
+    undifinedUserIDInput.value = "";
+    undifinedUserPWInput.value = "";
+  } else {
+    undifinedUserIDInput.value = "";
+    alert("This ID is already exist");
+  }
+}
+
+signupForm.addEventListener("submit", signupUserInfo);
