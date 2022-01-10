8a9ecff2b0514b2cbb5891f22f2644a9b5bbae1a
kimsongsub <busanbak2000@naver.com>
2022-01-10 20:57:40 +0900

    update todo function
    add remove function that removes clicked todoList

--- js/todo.js
+++ js/todo.js
@@ -2,12 +2,24 @@ const todoForm = document.querySelector("#todo-form");
 const todoInput = todoForm.querySelector("#todo-input");
 const todoList = document.querySelector("#todo-list");
 
+function deleteBtn(event) {
+  const listOfTodo = event.target.parentElement;
+  listOfTodo.remove();
+}
+
 function addTodoList(todoInputValue) {
   const listOfTodo = document.createElement("li");
-  const contentOfTodo = document.createElement("span");
   todoList.appendChild(listOfTodo);
+
+  const contentOfTodo = document.createElement("span");
   listOfTodo.appendChild(contentOfTodo);
   contentOfTodo.innerText = todoInputValue;
+
+  const todoDeleteBtn = document.createElement("button");
+  listOfTodo.appendChild(todoDeleteBtn);
+  todoDeleteBtn.innerText = "✅";
+
+  todoDeleteBtn.addEventListener("click", deleteBtn);
 }
 
 function handleTodoSubmit(event) {
