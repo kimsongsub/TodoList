05fa57beb98e1d9eb1f0b9a8077d37751fbc9ff8
kimsongsub <busanbak2000@naver.com>
2022-01-12 00:25:51 +0900

    Revert "Update todo.js"
    This reverts commit e85626320e8238d9211731da365f7ac6b29761ec.

--- js/todo.js
+++ js/todo.js
@@ -2,56 +2,35 @@ const todoForm = document.querySelector("#todo-form");
 const todoInput = todoForm.querySelector("#todo-input");
 const todoList = document.querySelector("#todo-list");
 
-const LOCALSTORAGE_TODO_KEY = "Todos";
-loadSavedTodos();
-
 function deleteBtn(event) {
-  const liOfTodo = event.target.parentElement;
-  const spanOfTodo = liOfTodo.querySelector("span").innerText;
-  todos.splice(todos.indexOf(spanOfTodo), 1);
   // let ll = todos.filter((deleteElement) => {
   //   return spanOfTodo !== deleteElement;
   // });
   // todos = ll;
-  saveTodo();
-  liOfTodo.remove();
+  const listOfTodo = event.target.parentElement;
+  listOfTodo.remove();
 }
 
-function printTodos(todoInputValue) {
-  const liOfTodo = document.createElement("li");
-  todoList.appendChild(liOfTodo);
+function addTodoList(todoInputValue) {
+  const listOfTodo = document.createElement("li");
+  todoList.appendChild(listOfTodo);
 
-  const spanOfTodo = document.createElement("span");
-  liOfTodo.appendChild(spanOfTodo);
-  spanOfTodo.innerText = todoInputValue;
+  const contentOfTodo = document.createElement("span");
+  listOfTodo.appendChild(contentOfTodo);
+  contentOfTodo.innerText = todoInputValue;
 
-  const buttonOfDeleteTodo = document.createElement("button");
-  liOfTodo.appendChild(buttonOfDeleteTodo);
-  buttonOfDeleteTodo.innerText = "✅";
+  const todoDeleteBtn = document.createElement("button");
+  listOfTodo.appendChild(todoDeleteBtn);
+  todoDeleteBtn.innerText = "✅";
 
-  buttonOfDeleteTodo.addEventListener("click", deleteBtn);
+  todoDeleteBtn.addEventListener("click", deleteBtn);
 }
-
-function saveTodo() {
-  localStorage.setItem(LOCALSTORAGE_TODO_KEY, JSON.stringify(todos));
-}
-
-let todos = [];
 
 function handleTodoSubmit(event) {
   event.preventDefault();
   const todoInputValue = todoInput.value;
   todoInput.value = "";
-  todos.push(todoInputValue);
-  saveTodo(todoInputValue);
-  printTodos(todoInputValue);
+  addTodoList(todoInputValue);
 }
 
 todoForm.addEventListener("submit", handleTodoSubmit);
-
-function loadSavedTodos() {
-  const savedTodos = JSON.parse(localStorage.getItem(LOCALSTORAGE_TODO_KEY));
-  if (savedTodos) {
-    savedTodos.forEach(printTodos);
-  }
-}
