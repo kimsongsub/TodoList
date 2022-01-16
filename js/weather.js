8648e534d7ff658318a6b1cb87924b0790c8a24b
kimsongsub <busanbak2000@naver.com>
2022-01-16 17:43:17 +0900

    update weather
    add weather info where user at

--- js/weather.js
+++ js/weather.js
@@ -1,11 +1,20 @@
+const weather = document.querySelector("#weather span:first-child");
+const city = document.querySelector("#weather span:last-child");
 function onGetGeoSuccess(position) {
   const lat = position.coords.latitude;
   const lng = position.coords.longitude;
   console.log("you live in ", lat, lng);
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
 }
 
 function onGetGeoFail() {
-  alert("Can't find you. No weather for you");
+  alert("Can't find you. Please accept location Info");
 }
 
 navigator.geolocation.getCurrentPosition(onGetGeoSuccess, onGetGeoFail);
