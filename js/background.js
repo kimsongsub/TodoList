5b677a59e6c3afa2df3833adc7d628773f436dc3
kimsongsub <busanbak2000@naver.com>
2022-01-26 00:00:36 +0900

    hide img name

--- js/background.js
+++ js/background.js
@@ -1,10 +1,4 @@
-const imgName = [
-  "erol-ahmed-_CeW6TRUWws-unsplash.jpg",
-  "jisun-han-j0FFILuZPms-unsplash.jpg",
-  "mostafa-meraji-2DZ9vXU2bHo-unsplash.jpg",
-  "pavel-nekoranec-uEOA8TCQ_V8-unsplash.jpg",
-  "stefania-chihaia-k8j_c0stpt8-unsplash.jpg",
-];
+import { imgName } from "./setup.js";
 
 const randomImg = imgName[Math.floor(Math.random() * imgName.length)];
 
