e77b90966e4c76f74044a9c6386231439d98c30e
kimsongsub <busanbak2000@naver.com>
2022-02-21 15:59:08 +0900

    Update background.js
    publicize background img source

--- js/background.js
+++ js/background.js
@@ -1,5 +1,13 @@
 import { imgName } from "./setup.js";
 
+const imgName = [
+  "erol-ahmed-_CeW6TRUWws-unsplash.jpg",
+  "jisun-han-j0FFILuZPms-unsplash.jpg",
+  "mostafa-meraji-2DZ9vXU2bHo-unsplash.jpg",
+  "pavel-nekoranec-uEOA8TCQ_V8-unsplash.jpg",
+  "stefania-chihaia-k8j_c0stpt8-unsplash.jpg",
+];
+
 const randomImg = imgName[Math.floor(Math.random() * imgName.length)];
 
 const currentImg = document.createElement("img");
